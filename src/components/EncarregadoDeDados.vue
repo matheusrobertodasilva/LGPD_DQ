e1f8e89065af11dc250c79f307a204017e10d27f
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-06 17:41:01 -0300

    desenvolvendo componente de encarregado de dados

--- src/components/EncarregadoDeDados.vue
+++ src/components/EncarregadoDeDados.vue
@@ -2,5 +2,13 @@
 </script>
 
 <template>
-
+    <main class="h-[100vh] flex justify-center items-center my-auto">
+        <section class="lg:max-w-[1136px] lg:mx-auto mt-[34px] space-y-10 px-[14px]">
+            <div class="mb-[38px] space-y-5">
+                <h2 class="text-[28px] font-bold text-center">Encarregado de Dados</h2>
+                <p class="">Em atenção ao disposto no artigo 41 da Lei nº 13.709, a Prefeitura de Duque de Caxias institui a Portaria nº 569/2021 na qual designa o Encarregado de Dados Pessoais do Poder Executivo.</p>
+            </div>
+            <a href="#" class="p-5 bg-[rgb(0,71,143,1)] text-white font-bold rounded-xl">Portaria</a>
+        </section>
+    </main>
 </template>

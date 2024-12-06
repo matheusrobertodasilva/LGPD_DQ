c9a8e72c90979f6dfc56c79e84624fb69b0e3c79
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-06 17:46:26 -0300

    Criando componente Main

---
+++ src/components/Main.vue
@@ -0,0 +1,131 @@
+<script>
+</script>
+
+<template>
+    <main class="">
+        <section class="h-[100vh] bg-[url('src/assets/LGPD.png')] bg-no-repeat bg-cover bg-center text-white flex flex-col justify-center px-[10px] lg:px-20">
+            <h1 class="text-2xl lg:text-5xl font-bold ">LGPD</h1>
+            <h2 class="text-2xl lg:text-4xl font-bold my-[14px] lg:mb-10">LEI GERAL DE PROTEÇÃO DE DADOS PESSOAIS</h2>
+            <button class="p-3 bg-blue-500 rounded-xl font-bold w-[32%] lg:w-[10%]">Saiba mais</button>
+        </section>
+        <div class="lg:max-w-[1136px] lg:mx-auto space-y-[38px] mt-[38px]">
+            <section class="px-[14px] space-y-[34px]">
+                <article class="space-y-5">
+                    <h2 class="text-[28px] font-bold text-center">O que é a LGPD?</h2>
+                    <div class="space-y-5 ">
+                        <img src="/src/assets/LGPD-Lei-geral-de-protecao-de-dados-min.png" alt="" class="rounded-xl lg: lg:mr-12">
+                        <div class="space-y-5">
+                            <p>
+                                A Lei Geral de Proteção de Dados - LGPD – Lei Federal nº 13.709/2018, está vigente
+                                na sua totalidade, desde agosto de 2021 e estabelece regras sobre o tratamento (utilização)
+                                de dados pessoais de pessoa natural (pessoa física), seja por meios físicos ou digitais,
+                                protegendo os direitos fundamentais de liberdade, privacidade e personalidade de qualquer
+                                indivíduo.
+                            </p>
+                            <p>
+                                A Lei traz um conjunto de boas práticas/ações para utilização responsável de
+                                dados pessoais. Essas boas práticas dizem respeito à capacitação e mudança de cultura
+                                das equipes de trabalho, documentos jurídicos e segurança da informação.
+                            </p>
+                        </div>
+                    </div>
+                </article>
+                <article class="space-y-5">
+                    <h2 class="text-[28px] font-bold text-center">O que muda com a LGPD?</h2>
+                    <div class="space-y-5 lg:space-y-0 lg:flex lg:">
+                        <!-- <img src="" alt="" class="rounded-3xl lg:w-[50%] lg:mr-12"> -->
+                        <div class="space-y-5">
+                            <p>
+                                Com a Lei Geral de Proteção de Dados, o cidadão tem uma legislação específica
+                                que garante a proteção de seus dados pessoais. Dados pessoais são atributos da
+                                personalidade (registros abstratos ou concretos) que, quando mensurados ou classificados,
+                                podem revelar informações pessoais, como interesses, preferências, necessidades, classe
+                                social, entre outros.
+                            </p>
+                            <p>
+                                Assim, meros fragmentos ou vestígios isolados que parecem à primeira vista
+                                insignificantes, quando associados, cruzados e processados, com ajuda de tecnologias ou
+                                sistemas computacionais, podem revelar um quadro completo da personalidade de cada
+                                pessoa. Por tal razão, é necessária uma proteção direcionada aos dados (e não somente à
+                                informação), a fim de evitar que esses sejam usados de forma ilícita ou desvirtuada (como
+                                fins de discriminação ou exploração).
+                            </p>
+                            <p>
+                                Para estar em conformidade com a Lei Geral de Proteção de Dados, tanto poder
+                                público quanto entes privados precisam, para poder tratar dados pessoais, se enquadrar em
+                                alguma das hipóteses que justifiquem o tratamento (bases legais), além de adotar uma série
+                                de medidas de salvaguardas e de cuidados (uso da segurança e gestão de riscos para
+                                impedir o vazamento, eliminação de dados desnecessários, transparência, entre outros).
+                                Portanto, órgãos públicos necessitam adotar medidas de adequação e conformidade.
+                            </p>
+                            <p>
+                                Por fim, dentre outras diretrizes expressas legislação, só será considerado
+                                justificado o tratamento de dados pessoais se esse observar todos os princípios
+                                estabelecidos no artigo 6º da LGPD, que são os seguintes: finalidade, adequação,
+                                necessidade, livre acesso, qualidade dos dados, transparência, segurança, prevenção, não
+                                discriminação, responsabilização e prestação de contas.
+                            </p>
+                        </div>
+                    </div>
+                </article>
+                <article class="space-y-5">
+                    <h2 class="text-[28px] font-bold text-center">Qual a relação da Lei com a administração pública?</h2>
+                    <div class="space-y-5">
+                        <p>
+                            A Lei Geral de Proteção de Dados busca dar uniformidade e segurança jurídica ao
+                            tema da proteção de dados pessoais. Nessa tentativa, a LGPD terá incidência não só no
+                            âmbito privado, mas também no poder público.
+                        </p>
+                        <p>
+                            No entanto, considerando que a relação entre administração pública e cidadão é
+                            diferente da relação entre ente privado e indivíduo, a LGPD destinou um capítulo próprio à
+                            esfera pública (arts. 23 a 30 da LGPD). É importante ressaltar que na maioria das vezes, o
+                            tratamento de dados feito pelo poder público decorre do cumprimento de seus deveres
+                            constitucionais e legais.
+                        </p>
+                        <img src="/src/assets/seguranca-de-dados.webp" alt="" class="rounded-xl lg: lg:mr-12 lg:col-span-5">
+                        <p>
+                            Ainda, ao mesmo tempo em que buscará promover a tutela da proteção dos dados
+                            pessoais, o poder público também deverá observar outros princípios como o da eficiência
+                            (art. 37 da CF) e o da transparência (Lei do Acesso à Informação nº12.527/2011). Tal
+                            peculiaridade sinaliza, de antemão, o grande desafio que a administração terá de enfrentar.
+                            Um ponto importante para salientar, é que a LAI e a LGPD não possuem divergências nem
+                            conflitos, pois elas são complementares. A LAI estabelece diretrizes de transparência,
+                            enquanto a Lei Geral de Proteção de Dados complementa com critérios para proteção na
+                            utilização de dados pessoais e sensíveis.
+                        </p>
+                        <p>
+                            Outro ponto crítico é o compartilhamento de dados entre órgãos da administração
+                            pública ou entre eles e a iniciativa privada para finalidades distintas da que motivou a coleta
+                            originária do dado. Tal compartilhamento pode violar os princípios da finalidade e da
+                            adequação previstos na LGPD, porém, é relevante sabermos que nenhuma política pública
+                            ou de transparência prevista em lei deve ser impedida pela Lei de Proteção de Dados,
+                            conforme a mesma versa em seu art. 33, inciso VII.
+                        </p>
+                    </div>
+                </article>
+                <article class="space-y-5">
+                    <h2 class="text-[28px] font-bold text-center">Fundamentos da Proteção de Dados</h2>
+                    <div class="space-y-5 lg: lg:">
+                        <div class="space-y-5">
+                            <p>
+                                A LGPD traz claramente quais são os fundamentos relacionados à proteção de dados
+                                pessoais, que servem para embasar toda e qualquer ação que envolva seu tratamento.
+                            </p>
+                        </div>
+                        <img src="/src/assets/fundamentosLGPD.png" alt="" class="rounded-xl lg: lg:mr-12">
+                    </div>
+                </article>
+            </section>
+            <section class="space-y-5 px-[14px]">
+                <h2 class="text-[28px] font-bold text-center">Veja mais informações</h2>
+                <p class="text-center">Caso queira saber mais, abaixo esta uma cartilha falando mais sobre LPGD e outro documentos oficiais: </p>
+                <div class="space-y-5 flex flex-col ">
+                    <a class="p-3 bg-blue-700 rounded-xl text-white" href="https://portal.duquedecaxias.rj.gov.br/storage/lgpd/PAxleHYjON1860bCxMMgExPehc4tLwGiSy8e3Qy1.pdf">CARTILHA ORIENTATIVA DE PROTEÇÃO DE DADOS</a>
+                    <a class="p-3 bg-blue-700 rounded-xl text-white" href="https://portal.duquedecaxias.rj.gov.br/storage/lgpd/V4UmlJ7fNOmdtkSBbVu8Ajad8DoxiLFJafWUMgUr.pdf">LEI MUNICIPAL 3385/2024 - POLÍTICA DE PROTEÇÃO DE DADOS</a>
+                    <a class="p-3 bg-blue-700 rounded-xl text-white" href="https://portal.duquedecaxias.rj.gov.br/storage/lgpd/jSrYvdtIFr4PMWzNlbe6zLIsoidDrJo8eQ6LSL2Q.pdf">DECRETO MUNICIPAL 8820/2024 - POLÍTICA DE CLASSIFICAÇÃO DA INFORMAÇÃO</a>
+                </div>
+            </section>
+        </div>
+    </main>
+</template>

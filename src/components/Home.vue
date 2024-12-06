e50a3f016119c0161112c35a5fcccdf119a1b902
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-06 17:37:50 -0300

    mudando estrutura do componente Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
-<script>
+<script setup>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue'
+
 </script>
 
 <template>
-
+   <Header/>
+   <router-view></router-view>
+   <Footer/>
 </template>

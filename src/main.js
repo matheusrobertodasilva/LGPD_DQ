e757943482fdd15eb49d612ad1fae6a2ad1241c7
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-06 17:42:33 -0300

    Adicionando rotas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Main from './components/Main.vue'
+import EncarregadoDeDados from './components/EncarregadoDeDados.vue'
+import PoliticaDePrivacidade from './components/PoliticaDePrivacidade.vue'
+import Contato from './components/Contato.vue'
+import Informacoes from './components/Informacoes.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Main},
+        { path: '/encarregadoDeDados', component: EncarregadoDeDados},
+        { path: '/politicaDePrivacidade', component: PoliticaDePrivacidade},
+        { path: '/contato', component: Contato},
+        { path: '/informacoes', component: Informacoes},
+    ]
+});
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app')

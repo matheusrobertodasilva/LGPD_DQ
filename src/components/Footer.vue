a26eba9ddd50fa3fcd2d5b269dbf0a6282cad803
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-06 17:45:42 -0300

    Criando componente Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<template>
+    <footer class="bg-[#00478F] text-white mt-10">
+        <div class="lg:max-w-[1136px] lg:mx-auto py-[20px] px-[14px] space-y-5">
+            <img src="/src/assets/logo.png" alt="logo da prefeitura " class="w-[50%] lg:w-[20%] mx-auto">
+            <div>
+                <p class="text-center">Desenvolvido pela Subsecretaria de Tecnologia da Informação</p>
+                <p class="text-center">Prefeitura Municipal de Duque de Caxias © 2024 - Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
+</template>

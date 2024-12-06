689260fb114d61206293ef9a08be137458d334e2
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-06 10:17:15 -0300

    criando componentes

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script>
+    export default{
+        data(){
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <header class="bg-blue-950 text-white">
+        <nav class="flex justify-between items-center relative p-5 ">
+            <img src="" alt="logo">
+            <ul class="bg-neutral-400 rounded-lg p-3 space-y-5 shadow-xl absolute right-[10px] top-[70px] lg:">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Encarregado de Dados</a></li>
+                <li><a href="#">Política de Privacidade</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Informações</a></li>
+            </ul>
+            <button class=""><img src="" alt="menu"></button>
+        </nav>
+    </header>
+</template>

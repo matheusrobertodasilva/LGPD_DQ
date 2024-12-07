6155cc92482d322119a3891176a8b8fa50e79691
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2024-12-07 13:15:11 -0300

    concluindo componente de Politica de privacidade

--- src/components/PoliticaDePrivacidade.vue
+++ src/components/PoliticaDePrivacidade.vue
@@ -2,5 +2,245 @@
 </script>
 
 <template>
-
+    <main>
+        <section class="lg:max-w-[1136px] lg:mx-auto px-[14px] mt-[34px] space-y-5">
+            <div class="space-y-[24px]">
+                <h1 class="text-[28px] font-bold text-center ">Política de Privacidade</h1>
+                <div class="space-y-5">
+                    <p>Este site é mantido e operado pela Prefeitura Municipal de Duque de Caxias, doravante denominada PMDC.</p>
+                    <p>
+                        Coletamos e utilizamos alguns dados pessoais que pertencem àqueles que utilizam e/ou visitam nosso site. Ao fazê-lo, agimos na qualidade de Controladores desses dados e estamos sujeitos às disposições e regramentos impostos pela Lei Federal n. 13.709/2018 (Lei Geral de Proteção de Dados Pessoais – LGPD).
+                    </p>
+                    <p>
+                        A PMDC se compromete a proteger a privacidade e a segurança dos visitantes dos sites sob nossa responsabilidade. Segue abaixo o conteúdo de nossa política de privacidade online. Entre em contato conosco, caso haja alguma dúvida ou para maiores esclarecimentos.
+                    </p>
+                </div>
+            </div>
+            <article class="space-y-5">
+                <h2 class="font-bold">1. Dados que coletamos e motivos da coleta</h2>
+                <p>
+                    Nossos sites coletam e utilizam alguns dados pessoais de nossos visitantes, de acordo com o
+                    disposto nesta seção. Algumas destas informações são explicitamente fornecidas quando do
+                    preenchimento voluntário de formulários e aplicativos disponibilizados. Outras são coletadas
+                    dinamicamente e de forma automática ao navegar pelas páginas dos nossos portais na
+                    Internet.
+                </p>
+                <p class="pl-2">
+                    <span class="font-bold">a)</span> Dados pessoais fornecidos de forma expressa pelo usuário: Para garantir que possamos nos
+                    comunicar corretamente com os visitantes, algumas informações serão coletadas para que
+                    sejam associadas a pessoa específica. A isso chamamos de “Informação pessoal”, cujo
+                    conteúdo pode incluir, de forma exemplificativa: nome, CPF, data de nascimento, endereço,
+                    número de telefone e endereço de e-mail.
+                </p>
+                <p>
+                    Nos nossos sites, a coleta quando ocorre, principalmente, nos formulários e nas aplicações que
+                    exijam interação com o visitante – formulário de requerimento de informações, sugestões de
+                    leis, reclamações etc.
+                </p>
+                <p class="pl-2">
+                    <span class="font-bold">b)</span> Dados coletados de forma dinâmica e automática: São registrados, sem que haja
+                    intervenção nem notificação ao visitante. São exemplos deste tipo de dado: o endereço IP do
+                    computador utilizado para acesso, a geolocalização e tipo do dispositivo (móvel ou desktop). 
+                    Estas informações são utilizadas para fins estatísticos de navegação no site e melhoria
+                    contínua do serviço oferecido.
+                </p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">2. Dados sensíveis</h2>
+                <p>
+                    Não serão coletados, em nenhuma hipótese, dados sensíveis de nossos visitantes, assim
+                    entendidos aqueles definidos no inciso II, art. 5º da Lei de Proteção de Dados Pessoais. Assim,
+                    não haverá coleta de dados sobre origem racial ou étnica, convicção religiosa, opinião política,
+                    filiação a sindicato ou a organização de caráter religioso, filosófico ou político, dado referente
+                    à saúde ou à vida sexual, dado genético ou biométrico, quando vinculado a uma pessoa
+                    natural.
+                </p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">3. Coleta de dados não previstos expressamente</h2>
+                <p>
+                    Eventualmente, outros tipos de dados, não previstos expressamente nesta Política de
+                    Privacidade, poderão ser coletados, desde que sejam fornecidos com o consentimento do
+                    visitante, ou, ainda, que a coleta seja permitida com fundamento em outra base legal prevista
+                    em lei, especialmente no que dispõe o inciso III, do Art. 7º, da LGPD.
+                </p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">4. Compartilhamento de dados pessoais com terceiros</h2>
+                <p>
+                    Não compartilhamos dados pessoais de visitantes com terceiros. Apesar disso, é possível que o
+                    façamos para cumprir determinação legal ou regulatória ou, ainda, para cumprir ordem
+                    expedida por autoridade pública competente.
+                </p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">5. Tempo de armazenamento de dados pessoais</h2>
+                <p>
+                    Os dados pessoais coletados pelos nossos sites são armazenados e utilizados pelo período que
+                    corresponda ao necessário para atingir as finalidades elencadas neste documento e que
+                    considere os direitos de seus titulares, os direitos do Controlador e as disposições legais ou
+                    regulatórias aplicáveis.
+                </p>
+                <p>
+                    Uma vez expirados os períodos de armazenamento dos dados pessoais, eles são removidos de
+                    nossas bases de dados ou anonimizados, salvo nos casos em que houver a possibilidade ou a
+                    necessidade de armazenamento em virtude de disposição legal ou regulatória.
+                </p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">6. Como o titular pode exercer seus direitos</h2>
+                <p>
+                    Para garantir que o visitante que pretende exercer seus direitos é, de fato, o titular dos dados
+                    pessoais objeto da requisição, poderemos solicitar documentos ou outras informações que
+                    possam auxiliar em sua correta identificação, a fim de resguardar nossos direitos e os direitos
+                    de terceiros. Isto somente será feito, porém, se for absolutamente necessário, e o requerente
+                    receberá todas as informações relacionadas.
+                </p>
+            </article><article class="space-y-5">
+                <h2 class="font-bold">7. Medidas de segurança no tratamento de dados pessoais</h2>
+                <p>
+                    Empregamos medidas técnicas e administrativas aptas a proteger os dados pessoais de
+                    acessos não autorizados e de situações de destruição, perda, extravio ou alteração desses
+                    dados.
+                </p>
+                <p>
+                    As medidas que utilizamos levam em consideração a natureza dos dados, o contexto e a
+                    finalidade do tratamento, os riscos que uma eventual violação geraria para os direitos e
+                    liberdades do usuário e os padrões atualmente empregados em entidades governamentais.
+                </p>
+                <p>
+                    Ainda que se adote tudo o que está tecnicamente ao alcance para se evitar incidentes de
+                    segurança, é possível que ocorra algum problema motivado, exclusivamente, por ato volitivo e
+                    criminoso de terceiro – a exemplo de ataques de hackers ou crackers ou, ainda, em caso de
+                    culpa exclusiva do usuário, quando, por exemplo, ele mesmo transfere seus dados à terceiros.
+                    Assim, embora sejamos, em geral, responsáveis pelos dados pessoais que mantemos, nos
+                    eximimos de responsabilidade caso ocorra uma situação excepcional como essas, sobre as
+                    quais não temos nenhum tipo de controle.
+                </p>
+                <p>
+                    De qualquer forma, a ocorrência de incidentes de segurança que gere ou possa gerar risco ou
+                    dano relevante para qualquer de nossos usuários, comunicaremos, sempre que possível, aos
+                    diretamente afetados, e à Autoridade Nacional de Proteção de Dados acerca do ocorrido, em
+                    conformidade com o disposto na Lei Geral de Proteção de Dados. 
+                </p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">8. Cookies</h2>
+                <div class="pl-2">
+                    <h3 class="font-semibold ">8.1  O que são cookies?</h3>
+                    <div class="pl-2">
+                        <p>
+                            Cookies são pequenos arquivos contendo dados que o site solicita ao seu navegador para
+                            armazenar no computador ou dispositivo móvel do visitante. O cookie permite que o site
+                            “lembre” suas ações ou preferências ao longo do tempo.
+                        </p>
+                        <p>
+                            A maioria dos navegadores da Internet aceitam cookies; entretanto, os usuários podem
+                            configurar seus navegadores para recusar certos tipos de cookies ou cookies específicos. Além
+                            disso, os usuários podem apagar os cookies a qualquer momento.
+                        </p>
+                    </div>
+                </div>
+                <div class="pl-2">
+                    <h3 class="font-semibold ">8.2 Por que usamos cookies?</h3>
+                    <div class="pl-2">
+                        <p>
+                            Usamos cookies para aprender como o visitante interage com o nosso conteúdo e para
+                            melhorar a experiência ao visitar nossos sites. Por exemplo, alguns cookies lembram o idioma
+                            do visitante ou preferências para que ele não tenha que efetuar estas escolhas repetidamente 
+                            sempre que visitar um dos nossos sites. Também usamos cookies para ajudar no rastreamento
+                            de IPs e geolocalização dos nossos visitantes.
+                        </p>
+                    </div>
+                </div>
+                <div class="pl-2">
+                    <h3 class="font-semibold ">8.3 Cookies Primários e Cookies de Terceiros</h3>
+                    <div class="pl-2 space-y-5">
+                        <p>Utilizamos tanto cookies primários quanto cookies de terceiros no nosso site.</p>
+                        <p>
+                            Cookies primários são cookies emitidos pelos domínios sob nossa administração (Exemplos:
+                            lgpd.mesquita.rj.gov.br, transparência.mesquita.rj.gov.br, mesquita.rj.gov.br, etc.) e
+                            geralmente são utilizados para identificar preferências e facilitar a funcionalidade básica do
+                            site.
+                        </p>
+                        <p>
+                            Cookies de terceiros pertencem e são administrados por outras empresas ou provedores de
+                            serviços da PMDC. Estes cookies podem ser necessários, por exemplo, para extrair dados de
+                            páginas mais visitadas (Google Analytics) ou exibição de conteúdo personalizado (Youtube) no
+                            site da PMDC.
+                        </p>
+                        <div class="lg:flex lg:  lg:my-0 border">
+                            <div class="lg:w-[30%]">
+                                <p class="font-bold bg-blue-500 p-3 text-center text-white">Cookie de terceiros</p>
+                                <p class="text-center my-10">Google Analytics</p>
+                            </div>
+                            <div class="lg:w-[30%]">
+                                <p class="font-bold bg-blue-500 p-3 text-center text-white">Descrição</p>
+                                <p class="text-center my-10">Utilizamos Google Analytics para entender como o usuário interage com o nosso site objetivando a melhora da experiência do visitante.</p>
+                            </div>
+                            <div class="lg:w-[40%]">
+                                <p class="font-bold bg-blue-500 p-3 text-center text-white">Link para Opt-out/política de privacidade específica</p>
+                                <p class="text-center my-10">https://tools.google.com/dlpage/gaoptout</p>
+                            </div>
+                        </div>
+                        <p>
+                            Cookies da Sessão são cookies temporários que são utilizados para lembrar do usuário
+                            visitante durante o curso da visita ao site e eles perdem a validade quando se fecha o
+                            navegador.
+                        </p>
+                        <p>
+                            Cookies Persistentes são utilizados para lembrar as preferências do visitante no site e
+                            permanecem no seu desktop ou dispositivo móvel mesmo depois de se fechar o navegador ou
+                            efetuar uma reinicialização. Usamos estes cookies para analisar o comportamento do usuário e
+                            estabelecer padrões, de modo que possamos melhorar a funcionalidade do nosso site.
+                        </p>
+                    </div>
+                </div>
+                <div class="pl-2">
+                    <h3 class="font-semibold ">8.4 Rejeitando e apagando Cookies</h3>
+                    <div class="pl-2 space-y-5">
+                        <p>
+                            O usuário pode escolher rejeitar ou bloquear todos ou tipos específicos de cookies
+                            configurados nos sites da PMM. Você pode modificar as suas preferências sobre cookies
+                            alterando as configurações do seu navegador. Observar que a maioria dos navegadores 
+                            aceitam cookies automaticamente. Portanto, se o usuário não quiser que cookies sejam
+                            utilizados, pode ser necessário apagar ou bloquear cookies ativamente.
+                        </p>
+                        <p>
+                            A rejeição ou desativação dos cookies, no entanto, pode afetar a disponibilidade de algumas
+                            ferramentas e funcionalidades dos nossos sites, comprometendo seu correto e esperado
+                            funcionamento. Outra consequência possível é a remoção das preferências do usuário que
+                            eventualmente tiverem sido salvas, prejudicando sua experiência.
+                        </p>
+                        <p>
+                            Ao visitar qualquer um dos nossos sites apagando ou rejeitando alguns cookies, você concorda
+                            que podemos utilizar aqueles que não foram apagados ou rejeitados.
+                        </p>
+                    </div>
+                </div>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">9. Contato</h2>
+                <p>
+                    Para esclarecer quaisquer dúvidas sobre esta Política de Privacidade ou sobre os dados
+                    pessoais que tratamos, entre em contato com nosso Encarregado de Proteção de Dados
+                    Pessoais, por algum dos canais mencionados abaixo:
+                </p>
+                <p>Email: <a href="#"></a></p>
+            </article>
+            <article class="space-y-5">
+                <h2 class="font-bold">10. Alterações nesta política</h2>
+                <p>A presente versão desta Política de Privacidade foi atualizada pela última vez em: 08/09/2021.</p>
+                <p>
+                    Reservamo-nos o direito de modificar, a qualquer momento, as presentes normas,
+                    especialmente para adaptá-las às eventuais alterações feitas em nosso site, seja pela
+                    disponibilização de novas funcionalidades, seja pela supressão ou modificação daquelas já
+                    existentes.
+                </p>
+            </article>
+            <div class="flex justify-center">
+                <a href="#" class="p-3 bg-blue-500 font-semibold text-white rounded-lg">Política de Privacidade</a>
+            </div>
+        </section>
+    </main>
 </template>
